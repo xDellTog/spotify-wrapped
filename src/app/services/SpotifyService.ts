950890c3776de710d3be802f3682f5183205abd3
Daniel Tognon <37959139+xDellTog@users.noreply.github.com>
2022-12-01 14:47:39 -0300

    fix | get top artists

--- src/app/services/SpotifyService.ts
+++ src/app/services/SpotifyService.ts
@@ -14,4 +14,20 @@ export default class SpotifyService {
             throw await response.json();
         });
     }
+    
+    static async getTopArtists(accessToken: string = '') {
+        if (!accessToken) return Promise.reject('Invalid Token!');
+
+        return await fetch('https://api.spotify.com/v1/me/top/artists?limit=10', {
+            method: 'GET',
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded',
+                'authorization': `Bearer ${accessToken}`,
+            },
+        }).then(async (response: Response) => {
+            if (response.ok) return await response.json();
+
+            throw await response.json();
+        });
+    }
 }
