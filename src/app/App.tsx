0a8c0c19d493aa609870c6880f193ce4350da44b
Daniel Tognon <37959139+xDellTog@users.noreply.github.com>
2022-12-01 12:20:42 -0300

    test | env var

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  // const [accessToken, setAccessToken] = useState<string>();
+  // const [expiresIn, setExpiresIn] = useState<number>();
+
+  // useEffect(() => {
+  //   const query: any = Object.fromEntries(new URLSearchParams(window.location.hash.replace('#', '?')));
+  //   console.log(query);
+  //   if (!!query.access_token) {
+  //     setAccessToken(query.access_token);
+  //     setExpiresIn(query.expires_in);
+  //     window.location.hash = '';
+  //   }
+  // }, []);
+
+  console.log(process.env.SPOTIFY_CLIENT_ID);
+
   return (
     <div>
       <h1>Spotify Wrapped</h1>
